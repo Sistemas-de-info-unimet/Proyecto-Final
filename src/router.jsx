1fdb5e63a348e283b3459bb6b2e363444b9daade
jose32005 <jose.sebastiano@correo.unimet.edu.ve>
2024-03-25 13:10:58 -0400

    "commit pag principal"

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import Loginpage from './pages/Loginpage';
-import Signuppage from './pages/Signuppage'
+import Signuppage from './pages/Signuppage';
+import Home from './pages/Home'
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <Loginpage />
+    },
+
+    {
+        path: '/',
+        element: <Home />
     },
 
     {
