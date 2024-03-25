fc0e546583a490697b4c7d0a18404105ae8bcfbe
Alessandra Torres <alessandra.torres@correo.unimet.edu.ve>
2024-03-25 16:09:34 -0400

    Tarjeta inicio de sesion

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -1,26 +1,33 @@
 import "./Loginpage.css"
 export default function Loginpage() {
     return (
-        <div className="container">
-        <div className="form-container">
-          <form>
-            <h1>Iniciar sesión</h1>
-            <label className="email">Correo electrónico</label>
-            <input type="email" id="email" name="email" required></input>
-            <label className="password">Contraseña</label>
-            <input type="password" id="password" name="password" required></input>
-            <button type="submit">Iniciar sesión</button>
-          </form>
-        </div>
-        <div className="image-container">
-          <img src="https://www.unimet.edu.ve/wp-content/uploads/2023/09/Ingenieria-de-Sistemas.jpg" alt="Imagen de fondo"></img>
-          <div className="text-container">
-            <h2>¿Aún no tienes cuenta?</h2>
-            <p>Crea tu cuenta en pocos pasos:</p>
-            <button type="button">Crear cuenta</button>
-          </div>
+<div className="container">
+  <div className="card">
+    <div className="image-container">
+      <img src="imagen.jpg" alt="Imagen de fondo"></img>
+      <div className="text-container">
+        <h2>¿Aún no tienes cuenta?</h2>
+        <p>Crea tu cuenta en pocos pasos:</p>
+        <button type="button">Crear cuenta</button>
       </div> 
+    </div>
+    <div className="form-container">
+      <h1>Iniciar sesión</h1>
+      <form>
+        <label className="email">Correo electrónico</label>
+        <input type="email" id="email" name="email" required></input>
+        <label className="password">Contraseña</label>
+        <input type="password" id="password" name="password" required></input>
+        <button type="submit">Iniciar sesión</button>
+      </form>
+      <div className="social-login">
+        <button type="button">Iniciar sesión con Google</button>
+        <button type="button">Iniciar sesión con Facebook</button>
       </div>
+      <p className="admin-link">¿Eres administrador? <a href="#">Inicia sesión aquí</a></p>
+    </div>
+  </div>
+</div>
     );
 
 }
