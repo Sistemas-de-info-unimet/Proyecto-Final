9d61929feeeaf4c589aec4984736f85b5a08ce97
Louis <carreno.luisdavid@gmail.com>
2024-03-30 12:19:18 -0400

    comments summit

---
+++ src/components/AddComment.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import firebase from 'firebase/app';
+import { db, auth } from '../Firebase';
+import { arrayUnion, updateDoc, doc } from 'firebase/firestore';
+
+const AddComment = ({id}) => {
+  const [comment, setComment] = useState('');
+
+  const handleCommentChange = (event) => {
+    setComment(event.target.value);
+  };
+
+  const handleCommentSubmit = async () => {
+    const user = auth.currentUser;
+    if (user && comment) {
+      await updateDoc(doc(db, "Agrupaciones", id), {
+        comentarios: arrayUnion({
+          nombre: user.displayName,
+          comment: comment,
+        }),
+      });
+    }
+  };
+  return (
+    <div>
+      <textarea value={comment} onChange={handleCommentChange(id)} placeholder="Escribe tu comentario aquí..."></textarea>
+        <button onClick={handleCommentSubmit} style={{
+            boxShadow: '0 4px #c1a23c',
+            color: '#5e4800',
+            backgroundColor: '#ffd95e',
+            textTransform: 'uppercase',
+            padding: '10px 20px',
+            borderRadius: '5px',
+            transition: 'all .2s ease',
+            fontWeight: '900',
+            cursor: 'pointer',
+            letterSpacing: '1px',
+      }}>Enviar</button>
+    </div>
+  );
+};
+
+export default AddComment;
