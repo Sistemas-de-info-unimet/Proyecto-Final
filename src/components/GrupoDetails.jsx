1df61ce6dd533c68c97b97f1fe51f6a97fe9580a
David30821945 <122551334+David30821945@users.noreply.github.com>
2024-03-31 03:40:05 -0400

    lista de participantes 1

--- src/components/GrupoDetails.jsx
+++ src/components/GrupoDetails.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { db } from '../Firebase';
-import { doc, getDoc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
+import { doc, getDoc, updateDoc, arrayUnion, arrayRemove, collection, query, where, getDocs } from 'firebase/firestore';
 import { useAuth } from '../contexts/AuthContext'; 
 import { auth } from '../Firebase';
 
@@ -9,39 +9,39 @@ function GrupoDetails() {
   const [perfilUsuario, setPerfilUsuario] = useState(null);
   const { id } = useParams(); 
   const [agrupacion, setAgrupacion] = useState(null);
-  const [afiliados, setAfiliados] = useState([]);
+  const [participantes, setParticipantes] = useState([]);
   const navigate = useNavigate();
-  const { currentUser, updateCurrentUserSuscripciones } = useAuth();
-  const [userId, setUserId] = useState('');
+  const { currentUser } = useAuth();
   const user = auth.currentUser;
 
   useEffect(() => {
-    const obtenerDatosagrupacion = async () => {
-      // Obtener información del agrupacion
-      const docRef = doc(db, "Agrupaciones", id);
-      const docSnap = await getDoc(docRef);
-      if (docSnap.exists()) {
-        setAgrupacion(docSnap.data());
-        const afiliadosPromesas = docSnap.data().afiliados.map(async (afiliadoId) => {
-          const afiliadoRef = doc(db, "Afiliado", afiliadoId);
-          const afiliadoSnap = await getDoc(afiliadoRef);
-          return afiliadoSnap.exists() ? afiliadoSnap.data() : null;
-        });
-        const afiliados = await Promise.all(afiliadosPromesas);
-        setAfiliados(afiliados.filter(afiliado => afiliado !== null));
-      } else {
-        navigate('/'); 
+    const obtenerDatosAgrupacion = async () => {
+      try {
+        const docRef = doc(db, 'Agrupaciones', id);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+          setAgrupacion(docSnap.data());
+          setParticipantes(docSnap.data().afiliados); // Establecer afiliados en el estado
+          const participantesPromesas = docSnap.data().afiliados.map(async (afiliadoId) => {
+            const afiliadoDocRef = doc(db, 'Estudiante', afiliadoId);
+            const afiliadoDocSnap = await getDoc(afiliadoDocRef);
+            return afiliadoDocSnap.exists() ? afiliadoDocSnap.data().nombre : null;
+          });
+          const participantesNombres = await Promise.all(participantesPromesas);
+          setParticipantes(participantesNombres.filter(nombre => nombre !== null));
+        } else {
+          navigate('/');
+        }
+      } catch (error) {
+        console.error('Error al obtener datos de la agrupación:', error);
       }
     };
+
     const obtenerPerfilUsuario = async () => {
       try {
-        
-
         if (user) {
           const docRef = doc(db, 'Estudiante', user.uid);
           const docSnap = await getDoc(docRef);
-          setUserId(user.uid);
-
           if (docSnap.exists()) {
             setPerfilUsuario(docSnap.data());
           } else {
@@ -56,7 +56,7 @@ function GrupoDetails() {
     };
 
     obtenerPerfilUsuario();
-    obtenerDatosagrupacion();
+    obtenerDatosAgrupacion();
   }, [id, navigate]);
 
   const handleSuscripcion = async () => {
@@ -68,16 +68,16 @@ function GrupoDetails() {
         // Si el estudiante ya está suscrito, se remueve la agrupación de sus suscripciones
         nuevasSuscripciones = perfilUsuario.suscripciones.filter(mId => mId !== id);
         await updateDoc(userDocRef, { suscripciones: arrayRemove(id) });
-
-        // Se remueve al usuario de la lista de afiliados
         await updateDoc(doc(db, 'Agrupaciones', id), {
-          afiliados: arrayRemove(currentUser.uid)});
+          afiliados: arrayRemove(currentUser.displayName)
+        });
+        
       } else {
         // Si el estudiante no está suscrito, se agrega la agrupación a sus suscripciones
         nuevasSuscripciones = perfilUsuario ? [...perfilUsuario.suscripciones, id] : [id];
         await updateDoc(userDocRef, { suscripciones: arrayUnion(id) });
         await updateDoc(doc(db, 'Agrupaciones', id), {
-          afiliados: arrayUnion(currentUser.uid)
+          afiliados: arrayUnion(currentUser.displayName)
         });
       }
 
@@ -92,41 +92,39 @@ function GrupoDetails() {
   };
 
   return (
-    console.log(agrupacion),
-    console.log(currentUser.uid),
+
     <div className="container">
       {agrupacion && (
         <>
           <div>
-
             <h1>{agrupacion.nombre}</h1>
             <p>{agrupacion.descripcion}</p>
-            
+          </div>
           <div>
+            <h3>MISION:</h3>
+            <p>{agrupacion.mision}</p>
+          </div>
           <div>
-                <h3>MISION:</h3>
-                <p>{agrupacion.mision}</p>
-              </div>
-              <div>
-                <h3>VISION:</h3>
-                <p>{agrupacion.vision}</p>
-              </div>
-              <button onClick={handleSuscripcion}>
-  {perfilUsuario.suscripciones.includes(id) ? 'Retirarse' : 'Unirse'}
-</button>
-              {afiliados.map((afiliado, index) => (
-                <div key={index} className="card-afiliado">
-                  <h2>{afiliado.nombre}</h2>
-
-                </div>
+            <h3>VISION:</h3>
+            <p>{agrupacion.vision}</p>
+          </div>
+          <button onClick={handleSuscripcion}>
+            {perfilUsuario && perfilUsuario.suscripciones && perfilUsuario.suscripciones.includes(id)
+              ? 'Retirarse'
+              : 'Unirse'}
+          </button>
+          <div>
+            <h3>PARTICIPANTES:</h3>
+            <ul>
+              {agrupacion.afiliados.map((nombre, index) => (
+                <li key={index}>{nombre}</li>
               ))}
-            </div>
+            </ul>
           </div>
         </>
       )}
     </div>
   );
 }
-
 
 export default GrupoDetails;
