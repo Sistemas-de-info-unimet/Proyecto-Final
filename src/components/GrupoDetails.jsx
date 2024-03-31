310cb673ab9ee6913dffb9e9d9178f502727f967
David30821945 <122551334+David30821945@users.noreply.github.com>
2024-03-31 02:27:18 -0400

    suscripcion a agrupacion

--- src/components/GrupoDetails.jsx
+++ src/components/GrupoDetails.jsx
@@ -3,13 +3,17 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { db } from '../Firebase';
 import { doc, getDoc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore';
 import { useAuth } from '../contexts/AuthContext'; 
+import { auth } from '../Firebase';
 
 function GrupoDetails() {
+  const [perfilUsuario, setPerfilUsuario] = useState(null);
   const { id } = useParams(); 
   const [agrupacion, setAgrupacion] = useState(null);
   const [afiliados, setAfiliados] = useState([]);
   const navigate = useNavigate();
   const { currentUser, updateCurrentUserSuscripciones } = useAuth();
+  const [userId, setUserId] = useState('');
+  const user = auth.currentUser;
 
   useEffect(() => {
     const obtenerDatosagrupacion = async () => {
@@ -29,46 +33,91 @@ function GrupoDetails() {
         navigate('/'); 
       }
     };
+    const obtenerPerfilUsuario = async () => {
+      try {
+        
 
+        if (user) {
+          const docRef = doc(db, 'Estudiante', user.uid);
+          const docSnap = await getDoc(docRef);
+          setUserId(user.uid);
+
+          if (docSnap.exists()) {
+            setPerfilUsuario(docSnap.data());
+          } else {
+            console.log('No se encontró un perfil de usuario con el ID especificado.');
+          }
+        } else {
+          console.log('No se ha iniciado sesión.');
+        }
+      } catch (error) {
+        console.error('Error al obtener el perfil de usuario:', error);
+      }
+    };
+
+    obtenerPerfilUsuario();
     obtenerDatosagrupacion();
   }, [id, navigate]);
 
-  const toggleAfiliacion = async () => {
-    if (!currentUser || !currentUser.docId) return;
-  
-    const userDocRef = doc(db, "Estudiantes", currentUser.docId);
-    let nuevasSuscripciones;
-  
-    if (currentUser.suscripciones.includes(id)) {
-      nuevasSuscripciones = currentUser.suscripciones.filter(mId => mId !== id);
-      await updateDoc(userDocRef, {
-        Suscripciones: arrayRemove(id)
-      });
-    } else {
-      nuevasSuscripciones = [...currentUser.suscripciones, id];
-      await updateDoc(userDocRef, {
-        Suscripciones: arrayUnion(id)
-      });
+  const handleSuscripcion = async () => {
+    try {
+      const userDocRef = doc(db, 'Estudiante', currentUser.uid);
+      let nuevasSuscripciones;
+
+      if (perfilUsuario && perfilUsuario.suscripciones && perfilUsuario.suscripciones.includes(id)) {
+        // Si el estudiante ya está suscrito, se remueve la agrupación de sus suscripciones
+        nuevasSuscripciones = perfilUsuario.suscripciones.filter(mId => mId !== id);
+        await updateDoc(userDocRef, { suscripciones: arrayRemove(id) });
+
+        // Se remueve al usuario de la lista de afiliados
+        await updateDoc(doc(db, 'Agrupaciones', id), {
+          afiliados: arrayRemove(currentUser.uid)});
+      } else {
+        // Si el estudiante no está suscrito, se agrega la agrupación a sus suscripciones
+        nuevasSuscripciones = perfilUsuario ? [...perfilUsuario.suscripciones, id] : [id];
+        await updateDoc(userDocRef, { suscripciones: arrayUnion(id) });
+        await updateDoc(doc(db, 'Agrupaciones', id), {
+          afiliados: arrayUnion(currentUser.uid)
+        });
+      }
+
+      // Actualizar el estado local con las nuevas suscripciones
+      setPerfilUsuario(prevPerfilUsuario => ({
+        ...prevPerfilUsuario,
+        suscripciones: nuevasSuscripciones
+      }));
+    } catch (error) {
+      console.error('Error al manejar la suscripción:', error);
     }
-    updateCurrentUserSuscripciones(nuevasSuscripciones);
   };
 
   return (
+    console.log(agrupacion),
+    console.log(currentUser.uid),
     <div className="container">
       {agrupacion && (
         <>
           <div>
+
             <h1>{agrupacion.nombre}</h1>
             <p>{agrupacion.descripcion}</p>
-            <button onClick={toggleAfiliacion}>
-              {currentUser.Suscripciones.includes(id) ? 'Retirarse' : 'Afiliarse'}
-            </button>
+            
           <div>
-              {videoafiliados.map((afiliado, index) => (
+          <div>
+                <h3>MISION:</h3>
+                <p>{agrupacion.mision}</p>
+              </div>
+              <div>
+                <h3>VISION:</h3>
+                <p>{agrupacion.vision}</p>
+              </div>
+              <button onClick={handleSuscripcion}>
+  {perfilUsuario.suscripciones.includes(id) ? 'Retirarse' : 'Unirse'}
+</button>
+              {afiliados.map((afiliado, index) => (
                 <div key={index} className="card-afiliado">
-                  <h2>{afiliado.titulo}</h2>
-                  <p>Género: {afiliado.genero}</p>
-                  <p>{afiliado.descripcion}</p>
+                  <h2>{afiliado.nombre}</h2>
+
                 </div>
               ))}
             </div>
