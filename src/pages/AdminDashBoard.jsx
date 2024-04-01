6846c11157dc4e4728ebb4f8919907577f58f0dd
Luis Soriano <l.soriano@correo.unimet.edu.ve>
2024-03-31 22:47:08 -0400

    creando grupo admin

--- src/pages/AdminDashBoard.jsx
+++ src/pages/AdminDashBoard.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Logo from '../assets/LogoOpenG.png'
 import Swal from 'sweetalert2';
-import {setDoc, doc} from "firebase/firestore";
+import {setDoc, doc, collection} from "firebase/firestore";
 import { db } from "../Firebase";
 import "./AdminDashBoard.css"
 
@@ -10,10 +10,6 @@ export default function AdminDashBoard(){
 
     const UnimetLogo = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfUjUIUOUr2zEwuhLh4Q_qziqXtIHwEyyMEwRXsK34aQ&s"
     
-    //*varianble declaration
-    const [adminEmail, setAdminEmail] = useState("")
-    const [adminPassword, setAdminPassword]  = useState("");
-
     const [groupName, setGroupName] = useState("")
     const [mission, setMission] = useState("")
     const [vision, setVision] = useState("")
@@ -21,13 +17,11 @@ export default function AdminDashBoard(){
     const [groupType, setGroupeType] = useState("")
     const [active, setActive] = useState(false)
     const [description, setDescription] = useState("")
+    const [photo, setPhoto] = useState("")
 
     const [search, setSearch] = useState("")
 
     //*handles
-    const handleAdminEmail = (e)=>{setAdminEmail(e.target.value)}
-    const handleAdminPassword = (e)=> {setAdminPassword(e.target.value);}
-
     const handleName = (e) =>{setGroupName(e.target.value)}
     const handleMission = (e ) =>{setMission(e.target.value)}
     const handleVision = (e)=> {setVision(e.target.value)} 
@@ -35,8 +29,9 @@ export default function AdminDashBoard(){
     const handleGroupType = (e) =>{setGroupeType(e.target.value)}
     const handleActive = (e) =>{setActive(e.target.checked)}
     const handleDescription = (e) =>{setDescription(e.target.value)}
+    const handlePhoto = (e) =>{setPhoto(e.target.value)}
 
-    const handleCreateGroup = async (groupName, mission, vision, contactEmail, groupType, active, description) =>{
+    const handleCreateGroup = async (groupName, mission, vision, contactEmail, groupType, active, description, photo) =>{
         const isValidEmail = validateEmail(contactEmail)
         if (!isValidEmail){
             Swal.fire({
@@ -47,23 +42,27 @@ export default function AdminDashBoard(){
             })
             return;
         }
-        try{
+
             const currentState = active==true? "activo":"desactivo"
-            await setDoc(doc(db,"Agrupaciones",),
-            {
-                afiliados:[],
-                comentarios:[],
-                contacto:contactEmail,
-                descripcion:description,
-                estado:currentState,
-                //Todo foto
-                mision:mission,
-                vision:vision,
-                name: groupName,
-                tipoDeGrupo:groupType
-            })
-        }catch(e){
-        }
+            // Crea una referencia a la colección "Agrupaciones"
+            const collectionRef = collection(db, "Agrupaciones");
+
+            // Crea una referencia al nuevo documento dentro de la colección
+            const docRef = doc(collectionRef);
+
+            // Establece los datos del documento
+            await setDoc(docRef, {
+            afiliados: [],
+            comentarios: [],
+            contacto: contactEmail,
+            descripcion: description,
+            estado: currentState,
+            foto: photo,
+            mision: mission,
+            vision: vision,
+            name: groupName,
+            tipoDeGrupo: groupType,
+            });
 
     }
     const handleSearch = (e) =>{setSearch(e.target.value)}
@@ -74,9 +73,9 @@ export default function AdminDashBoard(){
     }
     
     function validateEmail(email) {
-        const regex = /^[\w-.]+@correo\.unimet\.edu\.ve$/;
+        const regex = /^[\w-.]+@unimet\.edu\.ve$/; 
         return regex.test(email);
-    }
+      }
     
     return (
     <>
@@ -84,15 +83,7 @@ export default function AdminDashBoard(){
         <img src={Logo} alt="not found" />
         <img src={UnimetLogo} alt="not found" />
     </div>
-    <div className="AdminInfoSeccion">
-        <p>Información de admin</p>
-        <div className='conainerAdminImput'>
-            <input type="password" className="Password" value={adminPassword} placeholder="*******" onChange={handleAdminPassword} />
-            <input type="text" className="Email" value={adminEmail} onChange={handleAdminEmail} placeholder='admin email'/>
-        </div>
-        <button className="AndminInfoChange">Actualizar datos</button>
-    </div>
-    <hr className="line" />
+   
     <div className="GroupsSection">
         
         <form action="" className="CreateGroupForm" onSubmit={createGroup}>
@@ -131,11 +122,11 @@ export default function AdminDashBoard(){
             </div>
             <div>
                 <p>Selecione imagen(es)</p>
-                <input type="file" required accept="image/png, image/gif, image/jpeg"/>
+                <input type="text" value={photo} onChange={handlePhoto} required/>
             </div>
             </div>
 
-            <input type="submit" value="Crear agrupación" id='createG' />
+            <button type="submit" value="Crear agrupación" id='createG' />
         </form>
         <hr className="line" />
         <div className="ModifySeccion">
